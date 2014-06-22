49a99d5e8e281a930675c54e0e005115992274c3
Typhaon <davidgodwin788@gmail.com>
2014-06-21 21:20:35 -0400

    Migrations & Models
    Ran migrations to create Restaurants and Reviews tables, along with
    associated blank models.

---
+++ db/migrate/20140622010114_create_restaurants.rb
@@ -0,0 +1,14 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name, null: false
+      t.string :address, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.integer :zip_code, null: false, limit: 5
+      t.text :description
+      t.string :category
+      t.timestamps
+    end
+  end
+end
