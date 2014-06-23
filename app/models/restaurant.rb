76b079022d481c761374bef295daa6b72a83edab
Typhaon <davidgodwin788@gmail.com>
2014-06-22 20:36:03 -0400

    Validations and Associations
    Added basic validations and associations to models.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ActiveRecord::Base
+  has_many :reviews
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zip_code, presence: true
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ActiveRecord::Base
+  belongs to :restaurant
+
+  validates :rating, presence: true
+  validates :body, presence: true
 end
