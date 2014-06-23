b05c0b01502d4748093df50e73db92cca8aa8ac5
Typhaon <davidgodwin788@gmail.com>
2014-06-22 22:48:00 -0400

    Views and Controllers
    Filling out restaurant views and controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,36 @@
 class RestaurantsController < ApplicationController
+
+def new
+  @restaurant = Restaurant.new
+end
+
+
+def create
+  @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+    flash[:notice] = "Success!"
+    redirect_to restaurants_path
+  else
+    flash.now[:notice] = "Error!"
+    render :new
+  end
+end
+
+def show
+  @restaurant = Restaurant.find(params[:id])
+end
+
+
+def index
+  @restaurants = Restaurant.order(name: :asc)
+end
+
+
+
+private
+
+def restaurant_params
+  params.require(:restaurant).permit(:name, :address, :city, :state, :zip_code, :category, :description)
+end
+
 end
